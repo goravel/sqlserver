ed4f422e1b7f580cddcbd8e2fedb630711a9329a
ALMAS <almas1992@126.com>
2025-02-18 17:10:41 +0800

    chore: optimize error handling (#8)

--- facades/sqlserver.go
+++ facades/sqlserver.go
@@ -1,26 +1,24 @@
 package facades
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/goravel/framework/contracts/database/driver"
 
 	"github.com/goravel/sqlserver"
 )
 
-func Sqlserver(connection string) driver.Driver {
+func Sqlserver(connection string) (driver.Driver, error) {
 	if sqlserver.App == nil {
-		log.Fatalln("please register Sqlserver service provider")
-		return nil
+		return nil, fmt.Errorf("please register sqlserver service provider")
 	}
 
 	instance, err := sqlserver.App.MakeWith(sqlserver.Binding, map[string]any{
 		"connection": connection,
 	})
 	if err != nil {
-		log.Fatalln(err)
-		return nil
+		return nil, err
 	}
 
-	return instance.(*sqlserver.Sqlserver)
+	return instance.(*sqlserver.Sqlserver), nil
 }
