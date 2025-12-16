f95fb27263281484c6f70808735c379f847f3b3a
Bowen <hwbrzzl@gmail.com>
2025-12-16 17:31:10 +0800

    optimize setup

--- setup/setup.go
+++ setup/setup.go
@@ -11,6 +11,7 @@ import (
 )
 
 func main() {
+	setup := packages.Setup(os.Args)
 	config := `map[string]any{
         "host":     config.Env("DB_HOST", "127.0.0.1"),
         "port":     config.Env("DB_PORT", 3306),
@@ -27,56 +28,53 @@ func main() {
 
 	appConfigPath := path.Config("app.go")
 	databaseConfigPath := path.Config("database.go")
-	modulePath := packages.GetModulePath()
+	moduleImport := setup.Paths().Module().Import()
 	sqlserverServiceProvider := "&sqlserver.ServiceProvider{}"
 	driverContract := "github.com/goravel/framework/contracts/database/driver"
 	sqlserverFacades := "github.com/goravel/sqlserver/facades"
 	databaseConnectionsConfig := match.Config("database.connections")
 	databaseConfig := match.Config("database")
 
-	packages.Setup(os.Args).
-		Install(
-			// Add sqlserver service provider to app.go if not using bootstrap setup
-			modify.When(func(_ map[string]any) bool {
-				return !env.IsBootstrapSetup()
-			}, modify.GoFile(appConfigPath).
-				Find(match.Imports()).Modify(modify.AddImport(modulePath)).
-				Find(match.Providers()).Modify(modify.Register(sqlserverServiceProvider))),
+	setup.Install(
+		// Add sqlserver service provider to app.go if not using bootstrap setup
+		modify.When(func(_ map[string]any) bool {
+			return !env.IsBootstrapSetup()
+		}, modify.GoFile(appConfigPath).
+			Find(match.Imports()).Modify(modify.AddImport(moduleImport)).
+			Find(match.Providers()).Modify(modify.Register(sqlserverServiceProvider))),
 
-			// Add sqlserver service provider to providers.go if using bootstrap setup
-			modify.When(func(_ map[string]any) bool {
-				return env.IsBootstrapSetup()
-			}, modify.AddProviderApply(modulePath, sqlserverServiceProvider)),
+		// Add sqlserver service provider to providers.go if using bootstrap setup
+		modify.When(func(_ map[string]any) bool {
+			return env.IsBootstrapSetup()
+		}, modify.AddProviderApply(moduleImport, sqlserverServiceProvider)),
 
-			// Add sqlserver connection config to database.go
-			modify.GoFile(databaseConfigPath).Find(match.Imports()).Modify(
-				modify.AddImport(driverContract),
-				modify.AddImport(sqlserverFacades, "sqlserverfacades"),
-			).
-				Find(databaseConnectionsConfig).Modify(modify.AddConfig("sqlserver", config)).
-				Find(databaseConfig).Modify(modify.AddConfig("default", `"sqlserver"`)),
+		// Add sqlserver connection config to database.go
+		modify.GoFile(databaseConfigPath).Find(match.Imports()).Modify(
+			modify.AddImport(driverContract),
+			modify.AddImport(sqlserverFacades, "sqlserverfacades"),
 		).
-		Uninstall(
-			// Remove sqlserver connection config from database.go
-			modify.GoFile(databaseConfigPath).
-				Find(databaseConfig).Modify(modify.AddConfig("default", `""`)).
-				Find(databaseConnectionsConfig).Modify(modify.RemoveConfig("sqlserver")).
-				Find(match.Imports()).Modify(
-				modify.RemoveImport(driverContract),
-				modify.RemoveImport(sqlserverFacades, "sqlserverfacades"),
-			),
+			Find(databaseConnectionsConfig).Modify(modify.AddConfig("sqlserver", config)).
+			Find(databaseConfig).Modify(modify.AddConfig("default", `"sqlserver"`)),
+	).Uninstall(
+		// Remove sqlserver connection config from database.go
+		modify.GoFile(databaseConfigPath).
+			Find(databaseConfig).Modify(modify.AddConfig("default", `""`)).
+			Find(databaseConnectionsConfig).Modify(modify.RemoveConfig("sqlserver")).
+			Find(match.Imports()).Modify(
+			modify.RemoveImport(driverContract),
+			modify.RemoveImport(sqlserverFacades, "sqlserverfacades"),
+		),
 
-			// Remove sqlserver service provider from app.go if not using bootstrap setup
-			modify.When(func(_ map[string]any) bool {
-				return !env.IsBootstrapSetup()
-			}, modify.GoFile(appConfigPath).
-				Find(match.Providers()).Modify(modify.Unregister(sqlserverServiceProvider)).
-				Find(match.Imports()).Modify(modify.RemoveImport(modulePath))),
+		// Remove sqlserver service provider from app.go if not using bootstrap setup
+		modify.When(func(_ map[string]any) bool {
+			return !env.IsBootstrapSetup()
+		}, modify.GoFile(appConfigPath).
+			Find(match.Providers()).Modify(modify.Unregister(sqlserverServiceProvider)).
+			Find(match.Imports()).Modify(modify.RemoveImport(moduleImport))),
 
-			// Remove sqlserver service provider from providers.go if using bootstrap setup
-			modify.When(func(_ map[string]any) bool {
-				return env.IsBootstrapSetup()
-			}, modify.RemoveProviderApply(modulePath, sqlserverServiceProvider)),
-		).
-		Execute()
+		// Remove sqlserver service provider from providers.go if using bootstrap setup
+		modify.When(func(_ map[string]any) bool {
+			return env.IsBootstrapSetup()
+		}, modify.RemoveProviderApply(moduleImport, sqlserverServiceProvider)),
+	).Execute()
 }
